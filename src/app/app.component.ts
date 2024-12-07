5fbb45952370c8fd923a01565de7e5e6ca1a38b3
Kevin Trickey <trickeykevin@gmail.com>
2024-12-07 12:19:48 -0800

    2 screens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
@@ -8,6 +8,35 @@ import { HomeComponent } from './home/home.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor() { }
+
   title = 'kevintrickey-website';
+
+  ngOnInit(): void {
+    this.setupIntersectionObserver();
+  }
+
+  setupIntersectionObserver(): void {
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animate-fade-in-up');
+          observer.unobserve(entry.target);   // stop observing once animated
+        }
+      });
+    }, { threshold: 0.3 });
+
+    const elementsToAnimate = document.querySelectorAll('.scroll-animate');
+    elementsToAnimate.forEach(element => {
+      observer.observe(element);
+    });
+
+
+
+  }
+
+
+
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        olive: '#808000',
+      },
+      keyframes: {
+        'fade-in-up': {
+          '0%': { opacity: 0, transform: 'translateY(20px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        'fade-in-up': 'fade-in-up 0.6s ease-out',
+      },
+    },
   },
   plugins: [],
 }
