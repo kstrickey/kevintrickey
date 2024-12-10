600b3bc99a02c00c268d64ef94f442bc027d08c8
Kevin Trickey <trickeykevin@gmail.com>
2024-12-10 07:37:02 -0800

    fixed up second screen, ready to go

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,5 +14,18 @@ export class HomeComponent {
       element.scrollIntoView({ behavior: 'smooth' })
     }
   }
-  
+
+  navigateToNYT() {
+    window.open('https://www.nytimes.com/2022/03/24/crosswords/all-in-the-game-of-love.html', '_blank')
+  }
+
+  navigateToNPR() {
+    window.open('https://www.npr.org/sections/health-shots/2023/06/21/1183484892/no-kill-meat-grown-from-animal-cells-is-now-approved-for-sale-in-the-u-s', '_blank')
+  }
+
+  navigateToFracking() {
+    window.open('https://www.uchicagomedicine.org/forefront/research-and-discoveries-articles/increased-hospitalizations-for-heart-attacks', '_blank')
+  }
+
+
 }
